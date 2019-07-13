b075b865ae10fc3ffbe42969194a55f8d28e7198
alexpapay <alexpapay@mail.ru>
2019-07-13 23:16:12 +0200

    Menu beta version. +version for mobile.

--- Design/js/custom_script.js
+++ Design/js/custom_script.js
@@ -1,8 +1,20 @@
 // Menu scroll:
 $(window).on('scroll', function() {
-    if ($(window).scrollTop()) {
-        $('nav').addClass('black');
-    } else {
-        $('nav').removeClass('black');
+    if ($('nav').hasClass('showing') == true) {} else {
+        if ($(window).scrollTop()) {
+            $('nav').addClass('black');
+        } else {
+            $('nav').removeClass('black');
+        }
     }
 })
+
+// Mobile menu:
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+        $("nav").toggleClass("showing");
+        $("nav .contacts").toggle();
+        $("nav .menu").toggle();
+        $("nav .social").toggle();
+    });
+});
